7711b65278f3aa83d155b6b5cdf0170e4ce355fc
NPF Everyone <everyone@npfconsultores.pt>
2021-09-28 18:47:57 -0300

    migrations

--- 02_RestWithASPNETUdemy_ScaffoldViaTerminal/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
+++ 02_RestWithASPNETUdemy_ScaffoldViaTerminal/RestWithASPNETUdemy/RestWithASPNETUdemy/Startup.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.HttpsPolicy;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.Data.Sqlite;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,8 +14,11 @@ using RestWithASPNETUdemy.Business.Implementations;
 using RestWithASPNETUdemy.Model.Context;
 using RestWithASPNETUdemy.Repository;
 using RestWithASPNETUdemy.Repository.Implementations;
+using Serilog;
 using System;
 using System.Collections.Generic;
+using System.Data.Entity.Migrations;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -22,20 +26,30 @@ namespace RestWithASPNETUdemy
 {
     public class Startup
     {
-        public Startup(IConfiguration configuration)
+        public IWebHostEnvironment Environment { get; }
+        public IConfiguration Configuration { get; }
+
+        public Startup(IConfiguration configuration, IWebHostEnvironment environment)
         {
             Configuration = configuration;
+            Environment = environment;
+
+            Log.Logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
         }
 
-        public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-
             services.AddControllers();
 
             var connection = Configuration["SqlServerConnection:SqlServerConnectionString"];
+
+
+            if (Environment.IsDevelopment())
+            {
+                MigrateDataBase(connection);
+            }
 
             services.AddDbContext<SqlServerContext>(options => options.UseSqlServer(connection));
 
@@ -70,6 +84,26 @@ namespace RestWithASPNETUdemy
             {
                 endpoints.MapControllers();
             });
+        }
+
+        private void MigrateDataBase(string connection)
+        {
+            try
+            {
+                var cnx = new SqlConnection(Configuration["SqlServerConnection:SqlServerConnectionString"]);
+                var evolve = new Evolve.Evolve(cnx, msg => Log.Information(msg))
+                {
+                    Locations = new List<string> { "db/migrations", "db/dataset" },
+                    IsEraseDisabled = true
+                };
+
+                evolve.Migrate();
+            }
+            catch (Exception ex)
+            {
+                Log.Error("Database migration failed", ex);
+                throw;
+            }
         }
     }
 }
---
+++ 02_RestWithASPNETUdemy_ScaffoldViaTerminal/RestWithASPNETUdemy/RestWithASPNETUdemy/db/dataset/V2__Populate_Table_Person.sql
@@ -0,0 +1,3 @@
+INSERT [dbo].[person] ([address], [first_name], [gender], [last_name]) VALUES (N'Avenida Miguel Perrela 910', N'Crislaine', N'Female', N'Fernandes Silva Medeiros')
+INSERT [dbo].[person] ([address], [first_name], [gender], [last_name]) VALUES (N'Avenida Miguel Perrela 910', N'Breno', N'Male', N'Medeiros')
+INSERT [dbo].[person] ([address], [first_name], [gender], [last_name]) VALUES (N'Rua Margarida Praxedes Torres', N'Angela', N'Female', N'Medeiros')
